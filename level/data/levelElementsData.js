23e2b0b6aec6d849995755f49c89565641142573
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-02 09:28:48 +0900

    레벨 요소 관련 데이터 모듈화

---
+++ level/data/levelElementsData.js
@@ -0,0 +1,15 @@
+// 레벨 관련 요소
+
+const levelElementsData = {
+  // 레벨 요소
+  level : document.getElementById("level"),
+  // 전체 경험치 요소
+  allExp : document.getElementById("allExp"),
+  // 누적경험치 요소
+  accumulateExp : document.getElementById("accumulateExp")
+
+}
+
+
+// export
+export default levelElementsData
