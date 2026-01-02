0f4526e6ba47a19c5a6caeece142904bfa0583ec
nobamb <jsh12345644@gmail.com>
2026-01-02 18:00:24 +0900

    diary 클릭 테스트

--- diaryList/diaryGet.js
+++ diaryList/diaryGet.js
@@ -1,7 +1,8 @@
 // 일기의 특정 리스트를 클릭하면
 // 그 리스트의 내용을 메인화면으로 불러옴
 
-import clickOnOff from "../func/clickOnOff";
+import clickOnOff from "../func/clickOnOff.js";
+import diaryListElementData from "./data/diaryListElementData.js";
 
 // li의 h2(제목) => title
 // li의 span(날짜) => nowDate
@@ -13,8 +14,14 @@ import clickOnOff from "../func/clickOnOff";
 // saveButton off
 // updateButton, deleteButton off
 const diaryGet = () => {
+  // 테스트
+  diaryListElementData.diaryListChild.onclick = () => {
+
+    console.log("!!!")
+
+  }
   // diarylist의 li 클릭 시
-  clickOnOff()
+  // clickOnOff(diaryListElementData.diaryListChild)
   // mainheader, maincontext on
   // inputTitle, inputContext off
   // title, diaryContext on
