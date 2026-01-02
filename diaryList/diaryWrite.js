670be8f2b7e1bf580ff4eedaee4b55170df9048e
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-02 11:23:31 +0900

    writeWrite 관련해서 html 요소들 가져옴

---
+++ diaryList/diaryWrite.js
@@ -0,0 +1,23 @@
+// 일기 작성 버튼 관련 이벤트 동작 함수
+// 일기 작성 버튼을 클릭시, 오른쪽의 메인화면에 
+// 일기 작성관련 창이 뜸
+const diaryWrite = () => {
+
+  // 일기작성 버튼 writeDiaryButton 클릭시,
+  // mainContext, mainHeader가 on이 되면서
+  // 제목과 내용에 대해 작성을 할 수 있음
+  // writeDiaryButton 가져오기
+  const writeDiaryButton = document.getElementById("writeDiaryButton");
+  // mainContext 가져오기
+  const mainContext = document.getElementById("mainContext");
+  // mainHeader 가져오기
+  const mainHeader = document.getElementById("mainHeader")
+  
+  // writeDiary 클릭시 이벤트 발생
+  writeDiaryButton.onclick = () => {
+
+
+
+  }
+
+}
