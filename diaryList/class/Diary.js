46faea27c9b17dc5f6999b1e1b0d1990d6620ceb
nobamb <jsh12345644@gmail.com>
2026-01-02 16:38:22 +0900

    Diary 클래스에 date 추가

--- diaryList/class/Diary.js
+++ diaryList/class/Diary.js
@@ -1,9 +1,10 @@
 // 일기 관련 클래스
 export default class Diary{
-  // 제목, 내용을 받음
-  constructor(title, context){
+  // 제목, 내용, 날짜를 받음
+  constructor(title, context, date){
     this.title = title,
     this.context = context
+    this.date = date
   }
 
 }
