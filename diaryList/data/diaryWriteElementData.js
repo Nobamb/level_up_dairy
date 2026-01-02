5e538c74fc387cda5c6aea2eb66ce360b5659351
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-02 12:24:09 +0900

    diaryWrite관련 요소 데이터 추가(이전 커밋과 착각함, 이것이 진짜)

---
+++ diaryList/data/diaryWriteElementData.js
@@ -0,0 +1,12 @@
+// diaryWrite 요소 관련 데이터
+// 지금은 writeDiaryButton만 존재하지만,
+// 추후 동적으로 데이터를 받을 수 있도록 data 객체로 정리
+
+const diaryWriteElementData = {
+  // writeDiaryButton 가져오기
+  writeDiaryButton : document.getElementById("writeDiaryButton")
+
+}
+
+// export
+export default diaryWriteElementData
