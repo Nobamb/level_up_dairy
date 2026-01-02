13a03ca71be653638065ff3afc71d223edf6ea73
nobamb <jsh12345644@gmail.com>
2026-01-02 17:01:56 +0900

    diarylist 요소 import

--- mainScreen/func/diaryListAppend.js
+++ mainScreen/func/diaryListAppend.js
@@ -2,6 +2,10 @@
 import Diary from "../../diaryList/class/Diary.js";
 // diaryListData 가져옴
 import diaryListData from "../../diaryList/data/diaryListData.js";
+// diaryListElementData 가져옴
+import diaryListElementData from "../../diaryList/data/diaryListElementData.js";
+import diaryWriteElementData from "../../diaryList/data/diaryWriteElementData.js";
+
 
 // diary 리스트 데이터 추가 함수
 // 텍스트와 내용을 지정
@@ -20,11 +24,11 @@ const diaryListAppend = (titleValue, contextValue, date) => {
   const maxDiaryIndex = diaryListData.length - 1;
   // 테스트
   // 최근 작성한 데이터의 제목, 내용이 나오는지
-  console.log(diaryListData[maxDiaryIndex]);
+  // console.log(diaryListData[maxDiaryIndex]);
 
   // diaryList에 li로 값들 추가
   // diaryList에 li로 title이랑 context내용 추가
-  const diaryList = document.getElementById("diaryList");
+  const diaryList = diaryListElementData.diaryList
   // insertAdjectHTML 적용해보기
   // diaryList에 적용
   // li태그 추가
