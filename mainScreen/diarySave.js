f701cf86cfb8f8dfd547997f9d6e845715171009
nobamb <jsh12345644@gmail.com>
2026-01-02 15:15:37 +0900

    Diary 클래스, diaryListData, settingButtons 가져옴

--- mainScreen/diarySave.js
+++ mainScreen/diarySave.js
@@ -8,23 +8,37 @@
 // saveButton off,
 // updateButton, deleteButton on
 
+import Diary from "../diaryList/class/Diary.js";
+import diaryListData from "../diaryList/data/diaryListData.js";
 import onOff from "../func/onOff.js";
 import mainContextElementData from "./mainContext/data/mainContextElementData.js";
 import mainHeaderElementData from "./mainHeader/data/mainHeaderElementData.js";
+import settingButtons from "./settingButtons/data/buttonsElementsData.js";
 
 const diarySave = () => {
   // saveButton 가져옴
-  const saveButton = document.getElementById("saveButton");
+  const saveButton = settingButtons.saveButton;
   // saveButton 클릭시,
   saveButton.onclick = () => {
-
     // input으로 작성했던 title, context 출력(기능 동작 테스트)
     // console.log(mainHeaderElementData.inputTitle.value)
     // console.log(mainContextElementData.inputContext.value)
+    
     // 제목 값
     const titleValue = mainHeaderElementData.inputTitle.value
     // 내용 값
     const contextValue = mainContextElementData.inputContext.value
+    
+    // 새로 저장한 일기를 리스트에 추가
+    // 제목과 내용이 각각 들어감
+    const diary = new Diary(titleValue, contextValue)
+    // diarylist 전체 데이터에 값 push
+    diaryListData.push(diary)
+
+    // console 테스트
+    console.log(diaryListData)
+
+
     // inputTitle(제목 입력창), inputContext(내용 입력창)의 값을
     // title(제목), diaryContext(내용)에 적용
     mainHeaderElementData.title.innerHTML = titleValue;
@@ -38,6 +52,8 @@ const diarySave = () => {
       [title, diaryContext, updateButton, deleteButton],
       [inputTitle, inputContext, saveButton]
     );
+
+
   };
 };
 
