44fbbc5a0d846108a0008ce2a2d2f88418ea2b99
nobamb <jsh12345644@gmail.com>
2026-01-02 14:06:42 +0900

    dateformat 클래스 생성

---
+++ class/DateFormat.js
@@ -0,0 +1,18 @@
+// 날짜 포맷
+// 연, 월, 일 기준
+class DateFormat{
+  // 연,월,일을 받음
+
+  constructor(year, month, day) {
+    // year = 년
+    // month = 월
+    // day = 일
+    this.year = year,
+    this.month = month,
+    this.day = day
+  }
+
+}
+
+// export
+export default DateFormat
