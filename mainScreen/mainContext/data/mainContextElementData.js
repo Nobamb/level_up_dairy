6eb1f2eca088cf92d6917dfe79dc71ed9cf1c057
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-02 12:19:58 +0900

    mainContext 관련 요소 데이터 추가

---
+++ mainScreen/mainContext/data/mainContextElementData.js
@@ -0,0 +1,15 @@
+// mainContext 관련 데이터
+// mainContext와 관련된 요소들의 데이터를 저장
+
+const mainContextElementData = {
+  // mainContext 가져오기
+  mainContext: document.getElementById("mainContext"),
+  // inputContext 가져오기
+  inputContext: document.getElementById("inputContext"),
+  // diaryContext 가져오기
+  diaryContext: document.getElementById("diaryContext"),
+};
+
+
+// export 
+export default mainContextElementData
