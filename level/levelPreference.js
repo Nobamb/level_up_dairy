bf9e46167e72b7787cc32a97e2d0e08eb95b6296
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-02 09:24:10 +0900

    레벨 데이터 import

--- level/levelPreference.js
+++ level/levelPreference.js
@@ -2,13 +2,14 @@
 import Level from "../class/Level.js";
 // 레벨 요소 관련 import 
 import LevelElements from "../class/LevelElements.js";
-
+// 레벨 데이터 관련 import
+import levelData from "./data/levelData.js";
 
 // 레벨 관련 환경설정
 const levelPreference = () =>{
   
   // level, levelElements 생성자 생성
-  const level = new Level(1, 100)
+  const level = new Level(levelData.level, levelData.allExp)
   const levelElements = new LevelElements(document.getElementById("level"), document.getElementById("allExp"), document.getElementById("accumulateExp"))
   
   //  요소에 level 관련 값 지정
@@ -18,7 +19,7 @@ const levelPreference = () =>{
   levelElements.allExp.innerHTML = level.levelExpMaxValue;
   // 현재 경험치량 표시
   // 임의로 0 표시
-  levelElements.accumulateExp.innerHTML = 0;
+  levelElements.accumulateExp.innerHTML = levelData.accumulateExp;
   
   
   // 하루에 처음 접속했을 때,
