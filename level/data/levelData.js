fe78cc3a28db45e1eb50fe2dd0a7bd7e0286a2c7
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-02 09:23:57 +0900

    레벨 관련 데이터 생성

---
+++ level/data/levelData.js
@@ -0,0 +1,14 @@
+// 레벨 관련 데이터
+// 객체 형태(동적으로 갯수가 달라질 수 있기에)
+const levelData = {
+  // 현재 레벨
+  level : 1,
+  // 레벨 1 x 전체 경험치
+  allExp : 100,
+  // 누적 경험치
+  accumulateExp : 0
+
+}
+
+// export
+export default levelData
