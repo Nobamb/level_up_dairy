aee763cc491f5f6287be94fba53c86ed61f444c8
nobamb <jsh12345644@gmail.com>
2026-01-02 15:14:35 +0900

    setting 관련 버튼들 요소 추가

---
+++ mainScreen/settingButtons/data/buttonsElementsData.js
@@ -0,0 +1,12 @@
+// settingButtons와 관련된 요소들
+// 저장 saveButton
+// 수정 updateButton
+// 삭제 deleteButton
+const settingButtons = {
+  saveButton: document.getElementById("saveButton"),
+  updateButton: document.getElementById("updateButton"),
+  deleteButton: document.getElementById("deleteButton"),
+};
+
+// export
+export default settingButtons
