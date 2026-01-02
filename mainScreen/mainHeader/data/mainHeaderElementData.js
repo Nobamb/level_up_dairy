c046222390c28ae052d1726ddaa87b397b7f70e9
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-02 12:19:10 +0900

    mainHeader 관련 요소 데이터 추가

---
+++ mainScreen/mainHeader/data/mainHeaderElementData.js
@@ -0,0 +1,17 @@
+// mainHeader 관련 데이터
+// mainHeader와 관련된 요소들의 데이터를 저장
+const mainHeaderElementData = {
+
+  // mainHeader 가져오기
+  mainHeader : document.getElementById("mainHeader"),
+
+   // inputTitle 가져오기
+  inputTitle : document.getElementById("inputTitle"),
+
+  // title 가져오기
+  title : document.getElementById("title")
+
+}
+
+// export 
+export default mainHeaderElementData
