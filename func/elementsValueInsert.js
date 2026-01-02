71595f0ac9050439b12932a43e6a1b5ad271d062
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-02 11:05:02 +0900

    valueInsert 함수 이름 변경 => elementsValueInsert 이유: 첫번째 파라미터의 요소를 접근하려면 innerHTML은 파라미터의 내부에서 접근해야 되기 때문

---
+++ func/elementsValueInsert.js
@@ -0,0 +1,24 @@
+// 대입될 변수들을 전부 묶어서 또다른 묶여진 값에 대입함
+// 배열을 파라미터로 받음
+// inserteds : 값을 받을 파라미터(배열 형태)
+// values : 전체적인 값들(배열 형태)
+const elementsValueInsert = (inserteds, values) => {
+
+  // 값 입력 잘되는지 테스트
+  // console.log(inserteds)
+  // console.log(values)
+
+  // foreach 문을 통해 inserteds를 잘게 쪼갬
+  // foreach문으로 값과 인덱스를 모두 가져올 수 있기에
+  // inserted에 해당하는 값의 innerHTML을 values의 인덱스에 접근하여 대입
+  inserteds.forEach((inserted, index) => {
+    inserted.innerHTML = values[index]
+  });
+
+  return inserteds
+
+}
+
+
+// export 시킴
+export default elementsValueInsert
