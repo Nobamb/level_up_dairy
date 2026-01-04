806bf2d1cc8857c4e212c5607e47432a21a752b3
nobamb <jsh12345644@gmail.com>
2026-01-04 15:28:45 +0900

    newDiary true/false에 따라 동작 변경

--- mainScreen/diarySave.js
+++ mainScreen/diarySave.js
@@ -8,6 +8,7 @@
 // saveButton off,
 // updateButton, deleteButton on
 
+import NewDiary from "../data/newDiary.js";
 import onOff from "../func/onOff.js";
 import diaryListAppend from "./func/diaryListAppend.js";
 import mainContextElementData from "./mainContext/data/mainContextElementData.js";
@@ -19,20 +20,37 @@ const diarySave = () => {
   const saveButton = settingButtons.saveButton;
   // saveButton 클릭시,
   saveButton.onclick = () => {
+    
+    // 제목 값
+    const titleValue = mainHeaderElementData.inputTitle.value;
+    // 내용 값
+    const contextValue = mainContextElementData.inputContext.value;
+    // 날짜 값
+    const dateValue = mainHeaderElementData.nowDate.innerHTML;
+    // 현재 수정 버튼을 클릭한 상태에서 저장을 하면
+    // 기존 일기가 수정이 안되고 새 일기로 저장이 되는 상태
+    // 따로 true, false를 가르는 변수(newDiary)를 export한후
+    // true일때 diarySave는 저장할 시 새로운 일기를 추가,
+    // false일 땐 diarySave는 저장할 시 선택한 일기를 수정
+
+    // newDiary가 true일 때(새로운 일기를 저장시)
+    if(NewDiary.newDiary){
+      // 일기 추가
+      diaryListAppend(titleValue, contextValue, dateValue);
+
+    }
+    // false일 때(기존의 내용을 수정하려고 할 때)
+    else{
+      // 테스트
+      console.log('수정!')
+
+    }
+
+
+
     // input으로 작성했던 title, context 출력(기능 동작 테스트)
     // console.log(mainHeaderElementData.inputTitle.value)
     // console.log(mainContextElementData.inputContext.value)
-    
-    // 제목 값
-    const titleValue = mainHeaderElementData.inputTitle.value
-    // 내용 값
-    const contextValue = mainContextElementData.inputContext.value
-    // 날짜 값
-    const dateValue = mainHeaderElementData.nowDate.innerHTML
-    // 일기 추가
-    diaryListAppend(
-      titleValue, contextValue, dateValue
-    );
 
 
     // // 새로 저장한 일기를 리스트에 추가
