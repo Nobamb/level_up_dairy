b72abb564d8dc0ba9c3d56fbcad64ea87399b8fc
nobamb <jsh12345644@gmail.com>
2026-01-04 16:22:32 +0900

    import 확장자명 제대로 수정

--- func/newDiaryBooleanChange.js
+++ func/newDiaryBooleanChange.js
@@ -1,7 +1,7 @@
 // newDiary의 값을 true, 또는 false로 변경
 
 // newDiary를 가져옴
-import NewDiary from "../data/newDiary"
+import NewDiary from "../data/newDiary.js"
 
 // button : 클릭시 적용시킬 함수
 // bool : true 또는 false 적용시킬 값
