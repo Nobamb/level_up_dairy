97261da0c94ad177c732ab3d58fbb9ee73507cff
nobamb <jsh12345644@gmail.com>
2026-01-04 14:47:55 +0900

    updateButton에서 newDiary false로 바꾸기

--- mainScreen/diaryUpdate.js
+++ mainScreen/diaryUpdate.js
@@ -5,6 +5,7 @@
 // inputContext, inputTitle on
 // diaryContext, title off
 
+import newDiary from "../data/newDiary.js";
 import clickOnOff from "../func/clickOnOff.js";
 import mainContextElementData from "./mainContext/data/mainContextElementData.js";
 import mainHeaderElementData from "./mainHeader/data/mainHeaderElementData.js";
@@ -12,12 +13,15 @@ import settingButtons from "./settingButtons/data/buttonsElementsData.js";
 
 // title, diaryContext off
 const diaryUpdate = () => {
-  // 현재 수정 버튼을 클릭한 상태에서 저장을 하면
-  // 기존 일기가 수정이 안되고 새 일기로 저장이 되는 상태
-  // 따로 true, false를 가르는 변수(newDiary)를 export한후
-  // true일때 diarySave는 저장할 시 새로운 일기를 추가,
-  // false일 땐 diarySave는 저장할 시 선택한 일기를 수정
-  // 
+
+
+  // 업데이트 버튼(updateButton) 클릭 시,
+  // newDiary false
+  settingButtons.updateButton.onclick = () => {
+
+    newDiary = false
+
+  }
 
   // updateButton 클릭시
   // saveButton on
